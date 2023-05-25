d69fd81e103e3ed202be79c94ea916804aa49fcb
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-25 13:51:39 +0300

    Create project
    Delete storyboard

---
+++ Caring Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Caring Collection
+//
+//  Created by Sergei Semko on 5/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+

1317bd2add26621ddc4aacae0fe0422671391c7b
justSmK <28645140+justSmK@users.noreply.github.com>
2023-06-08 20:46:27 +0300

    Best solution

--- Caring Collection/SceneDelegate.swift
+++ Caring Collection/SceneDelegate.swift
@@ -18,7 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
+        let collectionViewNavigationController = UINavigationController(rootViewController: CollectionViewController())
+        collectionViewNavigationController.navigationBar.prefersLargeTitles = true
+        
+        window?.rootViewController = collectionViewNavigationController
         window?.makeKeyAndVisible()
     }
 
